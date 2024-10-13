28cfaf04eb8da05d6e56b4c81d0cbd8c6a36caac
Rafael Neves <nevesrafael.dev@gmail.com>
2024-10-13 20:44:39 -0300

    :sparkles: Feat authenticate controller

---
+++ src/controllers/authenticate.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Post } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+
+// const createAccountBodySchema = z.object({
+//   name: z.string(),
+//   email: z.string().email(),
+//   password: z.string(),
+// });
+
+// type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>;
+
+@Controller("/sessions")
+export class AuthenticateController {
+  constructor(private jwt: JwtService) {}
+
+  @Post()
+  // @HttpCode(201)
+  // @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  async handle() {
+    const token = this.jwt.sign({ sub: "user-id" });
+
+    return token;
+  }
+}
--- src/env.ts
+++ src/env.ts
@@ -3,6 +3,8 @@ import { z } from "zod";
 
 export const envSchema = z.object({
   DATABASE_URL: z.string().url(),
+  JWT_PRIVATE_KEY: z.string(),
+  JWT_PUBLIC_KEY: z.string(),
   NODE_ENV: z.enum(["dev", "test", "production"]).default("dev"),
   PORT: z.coerce.number().default(3333),
 });
